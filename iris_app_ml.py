e7eac47feb94c63ab8b7f2798de4422b316e25ab
rishabhsingh07 <81976745+rishabhsingh07@users.noreply.github.com>
2022-07-10 10:24:48 +0530

    Update iris_app_ml.py

--- iris_app_ml.py
+++ iris_app_ml.py
@@ -1,6 +1,7 @@
 from telnetlib import SB
 import streamlit as st
 import pandas as pd
+pip install scikit-learn
 import sklearn
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
